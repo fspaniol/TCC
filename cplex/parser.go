3bec457c1bd1bac201ea77535a324279352c223a
fspaniol <fspaniol@gmail.com>
2018-08-14 22:30:07 -0300

    Implement groups in order

--- cplex/parser.go
+++ cplex/parser.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 	"os"
 	"strconv"
@@ -15,8 +16,42 @@ type x struct {
 }
 
 var (
-	mapaX map[int][]x
+	groups map[int]*list.List
 )
+
+func handleX(name string, value string) {
+	val, _ := strconv.Atoi(value)
+	if val == 1 {
+		arr := strings.Split(name[2:len(name)-1], ",")
+		src := arr[0]
+		dst := arr[1]
+		flow, _ := strconv.Atoi(arr[2])
+		newX := x{
+			src: src,
+			dst: dst,
+		}
+		if groups[flow] == nil {
+			groups[flow] = list.New()
+		}
+		i := groups[flow].Front()
+		for i != nil {
+			elem := i.Value.(x)
+			if newX.src == elem.dst {
+				groups[flow].InsertAfter(newX, i)
+				break
+			} else {
+				if newX.dst == elem.src {
+					groups[flow].InsertBefore(newX, i)
+					break
+				}
+			}
+			i = i.Next()
+		}
+		if i == nil {
+			groups[flow].PushBack(newX)
+		}
+	}
+}
 
 func main() {
 	sol, err := parser.Transform(fmt.Sprintf("glpk/%s/%s.sol", os.Args[1], os.Args[1]))
@@ -24,27 +59,22 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	mapaX = make(map[int][]x)
+	groups = make(map[int]*list.List)
 
 	for _, v := range sol.Variables.Variable {
 		switch v.Name[0] {
 		case 'X':
-			src := strings.Split(v.Name[2:len(v.Name)-1], ",")[0]
-			dst := strings.Split(v.Name[2:len(v.Name)-1], ",")[1]
-			flow, _ := strconv.Atoi(strings.Split(v.Name[2:len(v.Name)-1], ",")[2])
-			val, _ := strconv.Atoi(v.Value)
-			newX := x{
-				src: src,
-				dst: dst,
-			}
-
-			if val == 1 {
-				mapaX[flow] = append(mapaX[flow], newX)
-			}
+			handleX(v.Name, v.Value)
 		}
 	}
 
-	for i := range mapaX {
-		fmt.Printf("Flow %v: %v\n", i, mapaX[i])
+	for i := range groups {
+		fmt.Printf("Flow %v: ", i)
+		j := groups[i].Front()
+		for j != nil {
+			fmt.Printf("%v ", j.Value)
+			j = j.Next()
+		}
+		fmt.Println()
 	}
 }
