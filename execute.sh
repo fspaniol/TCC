1e4086291bf5464599ef0ec1959c37c60523891e
fspaniol <fspaniol@gmail.com>
2018-08-14 22:41:56 -0300

    Track time since start

--- execute.sh
+++ execute.sh
@@ -1,12 +1,20 @@
 #/bin/bash
 
-# Usage, just call this script with the name of the test you want to run and then it will create the linear programming file, it will execute cplex and then convert it 
+# Usage, just call this script with the name of the test you want to run and then it will create the linear programming file, it will execute cplex and then convert it
+
+# Check the time when started to execute the script
+first=$SECONDS
 
 # Generate the linear file
-# glpsol -m glpk/into.mod -d glpk/$1/$1.dat --wlp glpk/$1/$1.lp --check
+glpsol -m glpk/into.mod -d glpk/$1/$1.dat --wlp glpk/$1/$1.lp --check
 
 # Execute cplex and put the output into the file folder
 # cplex/cplex -c "READ glpk/$1/$1.lp" "OPTIMIZE" "WRITE glpk/$1/$1.sol"
 
 # Execute the parser and put the output into the file folder as well
-go run cplex/parser.go $1 > glpk/$1/$1.txt 
+go run cplex/parser.go $1 > glpk/$1/$1.txt
+
+# Get the final time and write output to the file
+second=$SECONDS
+elapsed=`expr $second - $first`
+echo "Time taken running everything:" $elapsed "seconds" >> glpk/$1/$1.txt
