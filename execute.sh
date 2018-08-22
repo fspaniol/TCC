7fa017bfabaa03b68c10ec78acc81850d7952089
fspaniol <fspaniol@gmail.com>
2018-08-22 00:37:59 -0300

    Update script

--- execute.sh
+++ execute.sh
@@ -9,7 +9,7 @@ first=$SECONDS
 glpsol -m glpk/into.mod -d networks/$1/$1.dat --wlp networks/$1/$1.lp --check
 
 # Execute cplex and put the output into the file folder
-cplex/cplex -c "READ networks/$1/$1.lp" "SET timelimit 600" "OPTIMIZE" "WRITE networks/$1/$1.sol"
+cplex/cplex -c "READ networks/$1/$1.lp" "SET timelimit 600" "SET threads 4" "SET logfile networks/$1/$1-exec.txt" "SET output writelevel 3" "OPTIMIZE" "WRITE networks/$1/$1.sol"
 
 # Execute the parser and put the output into the file folder as well
 go run cplex/parser.go $1 > networks/$1/$1.txt
