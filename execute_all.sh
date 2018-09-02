a316e6d81adc15a0dd62a436ba7db3bc9d71ff46
fspaniol <fspaniol@gmail.com>
2018-09-01 21:45:48 -0300

    Add lower bound

--- execute_all.sh
+++ execute_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "| TEST NAME | NODE COUNT | LINK COUNT | FLOW COUNT | TIME RAN | SOLUTION FOUND | GAP (%)| LOWER BOUND |"
-echo "|---|:---:|:---:|:---:|:---:|:---:|:---:|:---:|"
+echo "| TEST NAME | NODE COUNT | LINK COUNT | FLOW COUNT | TIME RAN | SOLUTION FOUND | LOWER BOUND | GAP (%)| BRANCH | PROPORTION |"
+echo "|---|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|"
 
 for file in ./networks/*/; do
 	name=$(basename $file)
