da80ca59ec65526ad202872bf76f77eaae19939d
fspaniol <fspaniol@gmail.com>
2018-09-01 22:19:08 -0300

    Remove weight

--- names.sh
+++ names.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-for file in ./TopologyZoo/*
-do
-    nodes=$(head $file | grep "set V" | awk '{print $NF}' | tr -d ";")
-    #echo $nodes
+# for file in ./TopologyZoo/*
+# do
+#     nodes=$(head $file | grep "set V" | awk '{print $NF}' | tr -d ";")
+#     #echo $nodes
 
-    flows=$(cat $file | grep "set S" | awk '{print $NF}' | tr -d ";")
-    #echo $flows
+#     flows=$(cat $file | grep "set S" | awk '{print $NF}' | tr -d ";")
+#     #echo $flows
 
-    dir="zoo_${nodes}_${flows}"
-    mkdir $dir
+#     dir="zoo_${nodes}_${flows}"
+#     mkdir $dir
 
-    mv $file "${dir}/zoo_${nodes}_${flows}.dat"
+#     mv $file "${dir}/zoo_${nodes}_${flows}.dat"
+# done
+
+for f in networks/*/*.dat; do
+    echo $f
+    sed -i 'd/param q*/' $f
 done
-
