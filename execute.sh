8c4fe83cb3069d81004a7cd494bd7c0b98525325
fspaniol <fspaniol@gmail.com>
2018-09-08 23:50:34 -0300

    Prepare to run on cluster

--- execute.sh
+++ execute.sh
@@ -5,11 +5,13 @@
 # Check the time when started to execute the script
 #first=$SECONDS
 
+mkdir networks/$1/vrp
+
 # Generate the linear file
-#glpsol -m glpk/into-lower.mod -d networks/$1/input.dat --wlp networks/$1/standard/model.lp --check
+glpsol -m glpk/vrp.mod -d networks/$1/input.dat --wlp networks/$1/vrp/model.lp --check
 
 # Execute cplex and put the output into the file folder
-#cplex/cplex -c "READ networks/$1/standard/model.lp" "SET timelimit 3600" "SET threads 4" "SET logfile networks/$1/standard/exec.txt" "SET output writelevel 3" "OPTIMIZE" "WRITE networks/$1/standard/solution.sol" "y"
+cplex/cplex -c "READ networks/$1/vrp/model.lp" "SET timelimit 600" "SET threads 4" "SET logfile networks/$1/vrp/exec.txt" "SET output writelevel 3" "OPTIMIZE" "WRITE networks/$1/vrp/solution.sol" "y"
 
 #sed -n '/set A := /,/;/p' networks/$1/input.dat | tr -d "setA:=;" >networks/$1/links.txt
 
@@ -24,20 +26,20 @@
 #elapsed=$(expr $second - $first)
 #echo "Time taken running everything:" $elapsed "seconds" >>networks/$1/standard/groups.txt
 
-links=$(wc -l <networks/$1/links.txt | sed -e 's/^[ \t]*//')
-nodes="$(expr $(sed '3q;d' networks/$1/input.dat | tr -cd ' ' | wc -m) - 2)"
-flows="$(cut -d'_' -f3 <<<$1)"
-sol=$(cat networks/$1/standard/groups.txt | grep "Number" | awk '{print $NF}')
-time_ran=$(cat networks/$1/standard/exec.txt | grep "Total (root+branch&cut)" | awk '{print $4}')
-gap=$(cat networks/$1/standard/exec.txt | grep "(gap =" | awk '{print $NF}' | tr -d ")")
-lower=$(cat networks/$1/lower/groups.txt | grep "Number" | awk '{print $NF}')
-iterations=$(cat networks/$1/standard/exec.txt | grep "Iterations" | awk '{print $8}')
-proportion=$(bc <<<"scale=2; $sol/$lower")
+# links=$(wc -l <networks/$1/links.txt | sed -e 's/^[ \t]*//')
+# nodes="$(expr $(sed '3q;d' networks/$1/input.dat | tr -cd ' ' | wc -m) - 2)"
+# flows="$(cut -d'_' -f3 <<<$1)"
+# sol=$(cat networks/$1/standard/groups.txt | grep "Number" | awk '{print $NF}')
+# time_ran=$(cat networks/$1/standard/exec.txt | grep "Total (root+branch&cut)" | awk '{print $4}')
+# gap=$(cat networks/$1/standard/exec.txt | grep "(gap =" | awk '{print $NF}' | tr -d ")")
+# lower=$(cat networks/$1/lower/groups.txt | grep "Number" | awk '{print $NF}')
+# iterations=$(cat networks/$1/standard/exec.txt | grep "Iterations" | awk '{print $8}')
+# proportion=$(bc <<<"scale=2; $sol/$lower")
 
-if [ "$gap" == "" ] && [ "$sol" != "" ]; then
-	gap="0.00%"
-fi
+# if [ "$gap" == "" ] && [ "$sol" != "" ]; then
+# 	gap="0.00%"
+# fi
 
 # # Format of the table
 # # "| TEST NAME | NODE COUNT | LINK COUNT | FLOW COUNT | TIME RAN | SOLUTION FOUND | LOWER BOUND | GAP | PROPORTION | ITERATIONS |"
-echo "|$(echo $1 | sed 's/_/\\_/g')|$nodes|$links|$flows|$time_ran|$sol|$lower|$gap|$proportion|$iterations|"
+# echo "|$(echo $1 | sed 's/_/\\_/g')|$nodes|$links|$flows|$time_ran|$sol|$lower|$gap|$proportion|$iterations|"
--- execute_all.sh
+++ execute_all.sh
@@ -3,7 +3,7 @@
 echo "| TEST NAME | NODE COUNT | LINK COUNT | FLOW COUNT | TIME RAN | SOLUTION FOUND | LOWER BOUND | GAP (%)| PROPORTION | ITERATIONS |"
 echo "|---|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|"
 
-for file in ./networks/*/; do
+for file in ./networks/defo*/; do
 	name=$(basename $file)
 	source ./execute.sh $name
 done
