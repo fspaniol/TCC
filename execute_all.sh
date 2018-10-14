1d1e988d9e25ff7577b0e1518dc504d44b26a86b
fspaniol <fspaniol@gmail.com>
2018-10-14 00:20:43 -0300

    Create other networks folder

--- execute_all.sh
+++ execute_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "| TEST NAME | NODE COUNT | LINK COUNT | FLOW COUNT | INTO TIME RAN | INTO SOLUTION | VRP TIME RAN | VRP SOLUTION | LOWER BOUND | GAP (%)| PROPORTION | ITERATIONS |"
-echo "|---|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|"
+#echo "| TEST NAME | NODE COUNT | LINK COUNT | FLOW COUNT | INTO TIME RAN | INTO SOLUTION | VRP TIME RAN | VRP SOLUTION | LOWER BOUND | GAP (%)| PROPORTION | ITERATIONS |"
+#echo "|---|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|"
 
-for file in ./networks/*/; do
+for file in ./networks/zoo_??_*/; do
 	name=$(basename $file)
-	source ./execute.sh $name
+	source ./format_table.sh $name
 done
