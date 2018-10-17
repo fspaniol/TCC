282bb2b246f8bd5e62130d6337aaa7be89075a4a
fspaniol <fspaniol@gmail.com>
2018-10-17 00:01:35 -0300

    Update table

--- execute_all.sh
+++ execute_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "| TEST NAME | NODE COUNT | LINK COUNT | FLOW COUNT | COMPACT SOLUTION | COMPACT TIME | VRP SOLUTION | VRP TIME | LOWER SOLUTION | RELAX SOLUTION |"
+echo "| TEST NAME | NODE COUNT | LINK COUNT | FLOW COUNT | COMPACT SOLUTION | COMPACT TIME | VRP SOLUTION | VRP TIME | LOWER SOLUTION | LOWER2 |"
 echo "|---|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|"
 
 for file in ./networks/*/; do
--- format_table.sh
+++ format_table.sh
@@ -5,8 +5,8 @@ nodes="$(expr $(sed '3q;d' networks/$1/input.dat | tr -cd ' ' | wc -m) - 2)"
 flows="$(cut -d'_' -f3 <<<$1)"
 into_sol=$(cat networks/$1/standard/groups.txt | grep "Number" | awk '{print $NF}')
 vrp_sol=$(cat networks/$1/vrp/groups.txt | grep "Number" | awk '{print $NF}')
-into_time_ran=$(cat networks/$1/standard/exec.txt | grep "Total (root+branch&cut)" | awk '{print $4}')
-vrp_time_ran=$(cat networks/$1/vrp/exec.txt | grep "Total (root+branch&cut)" | awk '{print $4}')
+into_time_ran=$(cat networks/$1/standard/exec.txt | grep "Solution time =" | awk '{print $4}')
+vrp_time_ran=$(cat networks/$1/vrp/exec.txt | grep "Solution time =" | awk '{print $4}')
 lower=$(cat networks/$1/lower/groups.txt | grep "Number" | awk '{print $NF}')
 #lower_time_ran=$(cat networks/$1/lower/exec.txt | grep "Total (root+branch&cut)" | awk '{print $4}')
 lower2=$(cat networks/$1/lower2/groups.txt | grep "Number" | awk '{print $NF}')
