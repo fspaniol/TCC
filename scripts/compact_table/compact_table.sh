cbf80e4f4218df9b441f9a2c159790f71aa11f55
fspaniol <fspaniol@gmail.com>
2018-10-16 23:05:00 -0300

    Update times in values

--- scripts/compact_table/compact_table.sh
+++ scripts/compact_table/compact_table.sh
@@ -5,36 +5,30 @@ for file in ./networks/*/ ; do
     nodes="$(expr $(sed '3q;d' networks/$name/input.dat | tr -cd ' ' | wc -m) - 2)"
     links=$(wc -l <networks/$name/links.txt | sed -e 's/^[ \t]*//')
     flows="$(cut -d'_' -f3 <<<$name)"
-    compact_time=$(cat networks/$name/standard/exec.txt | grep "Total (root+branch&cut)" | awk '{print $4}')
+    compact_time=$(cat networks/$name/standard/exec.txt | grep "Solution time =" | awk '{print $4}')
     compact_gap=$(cat networks/$name/standard/exec.txt | grep "%" | awk '{print $NF}' | tail -n1 | tr -d "%)")
-    vrp_time=$(cat networks/$name/vrp/exec.txt | grep "Total (root+branch&cut)" | awk '{print $4}')
+    vrp_time=$(cat networks/$name/vrp/exec.txt | grep "Solution time =" | awk '{print $4}')
     vrp_gap=$(cat networks/$name/vrp/exec.txt | grep "%" | awk '{print $NF}' | tail -n1 | tr -d "%)")
-    lower_time=$(cat networks/$name/lower/exec.txt | grep "Total (root+branch&cut)" | awk '{print $4}')
+    lower_time=$(cat networks/$name/lower/exec.txt | grep "Solution time =" | awk '{print $4}')
     lower_gap=$(cat networks/$name/lower/exec.txt | grep "%" | awk '{print $NF}' | tail -n1 | tr -d "%)")
 
     if [ "$compact_time" == "" ]; then
         compact_time="0.00"
     fi
 
-    if [ "$compact_gap" == "" ] && [ "$compact_sol" != "" ]; then
+    if [ "$compact_gap" == "" ]; then
      	compact_gap="0.00"
-        echo "here"
     fi
 
-    if [ "$vrp_sol" == "" ]; then
+    if [ "$vrp_gap" == "" ]; then
         vrp_gap="0.00"
-        vrp_time="0.00"
-    else
-        if [ "$vrp_gap" == "" ]; then
-            vrp_gap="0.00"
-        fi
     fi
 
     if [ "$vrp_time" == "" ]; then
         vrp_time="0.00"
     fi
 
-    if [ "$lower_gap" == "" ] && [ "$lower_sol" != "" ]; then
+    if [ "$lower_gap" == "" ]; then
      	lower_gap="0.00"
     fi
 
