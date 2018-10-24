b9338b3d94181d3747a2ae6c4cb6397abc8e47b9
fspaniol <fspaniol@gmail.com>
2018-10-24 13:17:43 -0300

    Add plots for gap

--- create_plot.sh
+++ create_plot.sh
@@ -6,19 +6,19 @@
 rm -rf tmp
 mkdir tmp
 
-for file in ./networks/*_*_*/; do
+for file in ./networks/*/; do
 	name=$(basename $file)
 	
     links=$(wc -l <networks/$name/links.txt | sed -e 's/^[ \t]*//')
     nodes="$(expr $(sed '3q;d' networks/$name/input.dat | tr -cd ' ' | wc -m) - 2)"
     flows="$(cut -d'_' -f3 <<<$name)"
-    time=$(cat networks/$name/vrp/exec.txt | grep "Total (root+branch&cut)" | awk '{print $4}')
+    gap=$(cat networks/$name/vrp/exec.txt | grep "%" | awk '{print $NF}' | tail -n1 | tr -d "%)")
 
     val=`expr $links \* $nodes \* $flows`
     
-    if [ "$time" != "" ]; then
- 	    echo $val $time >> tmp/plot.txt
+    if [ "$gap" != "" ]; then
+ 	    echo $val $gap >> tmp/plot.txt
     fi
 done
 
-go run scripts/plot/generate.go > vrp.txt
+go run scripts/plot/generate.go > vrp-gap.txt
